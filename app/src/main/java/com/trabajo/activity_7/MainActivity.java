24c69ff9e7c61321778b978a19b068c85c11bae3
Israel Vergara <43155602+mn1016@users.noreply.github.com>
2019-04-27 20:47:38 -0500

    Create MainActivity.java

---
+++ app/src/main/java/com/trabajo/activity_7/MainActivity.java
@@ -0,0 +1,29 @@
+package com.trabajo.activity_7;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private final static int IMAGE_WIDTH = 400;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        iv = (ImageView) findViewById(R.id.iv);
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (imageUri != null) {
+                    new Worker(iv).execute(imageUri);
+                }
+            }
+        }
+    }
+}
